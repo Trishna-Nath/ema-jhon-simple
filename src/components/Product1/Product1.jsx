d8b9f9f97fedf69ac63eb83a295b69b2e315752b
Trishna-Nath <t.nathbd@gmail.com>
2023-03-27 10:54:40 -0700

    added dynamic data to shopping cart

--- src/components/Product1/Product1.jsx
+++ src/components/Product1/Product1.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product1.css';
 
 const Product1 = (props) => {
     const { img, name, seller, quantity, price, ratings } = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +16,9 @@ const Product1 = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product)}
+                className='btn-cart'>Add to cart
+                <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
 
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,12 +5,18 @@ import './Shop.css'
 const Shop = () => {
 
     const [products, setproducts] = useState([]);
+    const [cart,setcart] = useState([])
 
     useEffect(() => {
         fetch(' https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
             .then(res => res.json())
             .then(data => setproducts(data))
     }, [])
+
+    const handleAddToCart = (product) =>{
+        const newCart = [...cart, product];
+        setcart(newCart);
+     }
 
 
     return (
@@ -20,11 +26,13 @@ const Shop = () => {
                     products.map(product => <Product1
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product1>)
                 }
             </div>
             <div className="cart-container">
                 <h4>order summary</h4>
+                <p>Selected items: {cart.length}</p>
 
             </div>
         </div>
