1c3938e7124bda4b7140633f4350e6a71e6b576d
Trishna-Nath <t.nathbd@gmail.com>
2023-03-27 10:20:56 -0700

    added more style of dynamic style

--- src/components/Product1/Product1.jsx
+++ src/components/Product1/Product1.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './Product1.css';
 
 const Product1 = (props) => {
-    const {img,name,seller,quantity,price} =props.product;
+    const { img, name, seller, quantity, price, ratings } = props.product;
     return (
         <div className='product'>
-         <img src={img} alt="" />
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
+            <button className='btn-cart'>Add to cart</button>
         </div>
+
     );
 };
 
